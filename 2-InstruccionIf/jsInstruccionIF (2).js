40544c1e5c055a014e3c10b24d91456b0d3207a0
Fernando0810 <52495399+Fernando0810@users.noreply.github.com>
2019-07-10 12:25:02 -0300

    clase 333

---
+++ 2-InstruccionIf/jsInstruccionIF (2).js
@@ -0,0 +1,16 @@
+function mostrar()
+{
+//tomo la edad  
+ 
+var edad;
+
+edad=parseInt(document.getElementById("edad").value);
+
+if(edad>=18)
+{
+alert("sos mayor de edad");
+
+}
+
+
+}//FIN DE LA FUNCIÓN
---
+++ 2-InstruccionIf/jsInstruccionIF (3).js
@@ -0,0 +1,19 @@
+function mostrar()
+{
+//tomo la edad  
+
+   var edad;
+
+   edad=parseInt(document.getElementById("edad").value);
+
+   if(edad>=18)
+   {
+
+    alert("sos mayor de edad");
+   }
+   else
+   {
+       alert("sos menor jeje");
+   }
+
+}//FIN DE LA FUNCIÓN
